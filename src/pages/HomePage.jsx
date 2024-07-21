359ce2560ee84931633608282e2724da8906bb8e
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-07-21 17:30:51 +0200

    More styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "../styles/HomePage.module.css"; // Import the CSS module
+import videoSrc from "../assets/videos/ShortEaredOwl_background.mp4";
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
+      <video className={styles.backgroundVideo} autoPlay loop muted>
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className={styles.content}>
         <h1 className={styles.heading}>Hallo Janosch!</h1>
         <p>
